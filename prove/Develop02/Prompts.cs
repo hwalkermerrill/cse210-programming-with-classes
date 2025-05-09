41a1aac3d01e15931e500ffe61a7695a691c7b02
Harrison Merrill <hwalkermerrill@gmail.com>
2025-05-09 15:53:44 +0900

    revise promptbank grammar for new structure

--- prove/Develop02/Prompts.cs
+++ prove/Develop02/Prompts.cs
@@ -6,15 +6,15 @@ public class Prompts
 	{
 		// Create Prompt Bank;
 		List<string> PromptBank = new List<string>();
-		PromptBank.Add("Write about one interesting interaction you had today.");
-		PromptBank.Add("Write about how you saw the Lord's hand in your life today.");
-		PromptBank.Add("Write about the strongest emotional experience and reaction you had today.");
-		PromptBank.Add("Write about something you learned today.");
-		PromptBank.Add("Write about something you are grateful for today.");
-		PromptBank.Add("Write about something you wish you could've changed about today.");
-		PromptBank.Add("Write about something you wish you could tell a family member.");
+		PromptBank.Add("What is one interesting interaction you had today.");
+		PromptBank.Add("How have you seen the Lord's hand in your life today.");
+		PromptBank.Add("What was the strongest emotional experience and reaction you had today.");
+		PromptBank.Add("What is something you learned today.");
+		PromptBank.Add("What is something you are grateful for today.");
+		PromptBank.Add("What is something you wish you could've changed about today.");
+		PromptBank.Add("What is something you wish you could tell a family member.");
 		PromptBank.Add("Write about today as though you were writing a letter to a friend.");
-		PromptBank.Add("Write about the best thing you did or saw today.");
+		PromptBank.Add("What is the best thing you did or saw today.");
 
 		Random random = new Random();
 		int randomIndex = random.Next(PromptBank.Count);
