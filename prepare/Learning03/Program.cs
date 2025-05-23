890f7b42605a512ef66b0bca7487882512a45cc3
Harrison Merrill <hwalkermerrill@gmail.com>
2025-05-23 14:29:17 +0900

    create test functions

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction test1 = new Fraction();
+        Console.WriteLine(test1.GetFractionString());
+        Console.WriteLine(test1.GetDecimalValue());
+        // Expected output is "1/1" and 1.0
+
+        Fraction test2 = new Fraction(5);
+        Console.WriteLine(test1.GetFractionString());
+        Console.WriteLine(test1.GetDecimalValue());
+        // Expected output is "5/1" and 5.0
+
+        Fraction test3 = new Fraction(1, 5);
+        Console.WriteLine(test1.GetFractionString());
+        Console.WriteLine(test1.GetDecimalValue());
+        // Expected output is "1/5" and 0.2
+
+        Fraction test4 = new Fraction(100, 15);
+        Console.WriteLine(test1.GetFractionString());
+        Console.WriteLine(test1.GetDecimalValue());
+        // Expected output is "100/15" and 6.6>7
     }
 }
