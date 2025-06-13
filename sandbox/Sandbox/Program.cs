4fa28743641cd568768944d61ea2ac65fc200e83
Harrison Merrill <hwalkermerrill@gmail.com>
2025-06-13 11:18:24 +0900

    swap to loading thing

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Threading;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Sandbox World!");
-    }
+	static void Main()
+	{
+		int seconds = 30;
+		string[] spinner = { "|", "/", "-", "\\" };
+		int spinnerIndex = 0;
+
+		Console.CursorVisible = false;
+		Console.WriteLine();
+
+		for (int i = seconds; i >= 0; i--)
+		{
+			for (int j = 0; j < 10; j++) // 10 spinner steps per second
+			{
+				Console.SetCursorPosition(0, 0);
+				Console.Write($"[{spinner[spinnerIndex]}] Countdown: {i:D2}s ");
+				spinnerIndex = (spinnerIndex + 1) % spinner.Length;
+				Thread.Sleep(100); // 100ms per frame, 10 frames per second
+			}
+		}
+
+		Console.SetCursorPosition(0, 1);
+		Console.WriteLine("Done!");
+	}
 }
