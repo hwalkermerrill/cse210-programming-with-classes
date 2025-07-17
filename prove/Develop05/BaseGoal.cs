cb6ed33b759ad8f1e265dc9b23d198fcb941d22b
Harrison Merrill <hwalkermerrill@gmail.com>
2025-07-17 09:24:02 +0900

    swap points with exp

--- prove/Develop05/BaseGoal.cs
+++ prove/Develop05/BaseGoal.cs
@@ -8,22 +8,22 @@ namespace EternalQuest
     // attributes here, following _camelCase naming convention
     protected string _name;
     protected string _description;
-    protected int _pointValue;
+    protected int _expValue;
 
     // getters here
     public string GetName() { return _name; }
     public string GetDescription() { return _description; }
-    public int GetPointValue() { return _pointValue; }
+    public int GetExpValue() { return _expValue; }
 
     // methods here
     public abstract int RecordEvent();
     public abstract string DisplayGoal();
     public abstract bool IsComplete();
-    protected BaseGoal(string name, string description, int pointValue)
+    protected BaseGoal(string name, string description, int expValue)
     {
       _name = name;
       _description = description;
-      _pointValue = pointValue;
+      _expValue = expValue;
     }
   }
 }
