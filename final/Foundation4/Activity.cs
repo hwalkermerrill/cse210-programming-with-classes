f08e60e27203d4ca6cb7500a044e7be035d851b2
Harrison Merrill <hwalkermerrill@gmail.com>
2025-07-20 01:40:07 +0900

    create class skeletons for namespace referecing, create activity abstract

---
+++ final/Foundation4/Activity.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace ZamirasFitnessTracker
+{
+  public abstract class Activity
+  {
+    // attributes here
+    private DateTime _date;
+    private int _lengthMinutes;
+
+    // constructor here
+    public Activity(DateTime date, int lengthMinutes)
+    {
+      _date = date;
+      _lengthMinutes = lengthMinutes;
+    }
+
+    // methods here
+    public virtual double GetDistance() { return 0.0; }
+    public virtual double GetSpeed() { return 0.0; }
+    public virtual double GetPace() { return 0.0; }
+
+    public string GetSummary()
+    {
+      string dateStr      = _date.ToString("dd MMM yyyy");
+      string activityType = this.GetType().Name;
+      double distance     = GetDistance();
+      double speed        = GetSpeed();
+      double pace         = GetPace();
+
+      return
+        $"{dateStr} {activityType} ({_lengthMinutes} min) - " +
+        $"Distance: {distance:F1} miles, " +
+        $"Speed: {speed:F1} mph, " +
+        $"Pace: {pace:F1} min per mile";
+    }
+  }
+}
