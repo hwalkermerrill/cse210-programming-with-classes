fa1bca8a3ae93cad1a10795c87494f07ea9d6dce
Harrison Merrill <hwalkermerrill@gmail.com>
2025-07-20 00:30:33 +0900

    generate .cs files, begin writing and production

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -2,8 +2,8 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Foundation3 World!");
-    }
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hello Foundation3 World!");
+	}
 }
