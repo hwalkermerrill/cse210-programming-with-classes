e6e52176c58a909694c8451c8f1439aafc57afc7
Harrison Merrill <hwalkermerrill@gmail.com>
2025-07-20 00:35:48 +0900

    build out address .cs

---
+++ final/Foundation3/Address.cs
@@ -0,0 +1,29 @@
+namespace InheritingTabletopEvents
+{
+  public class Address
+  {
+		// attributes here, following _camelCase naming convention
+    private string _street;
+    private string _city;
+    private string _stateProvince;
+    private string _country;
+
+		// constructor here
+		public Address(string street, string city, string stateProvince, string country)
+		{
+			_street = street;
+			_city = city;
+			_stateProvince = stateProvince;
+			_country = country;
+		}
+
+		// getters here
+		public string GetFullAddress()
+		{
+			return
+				$"{_street}\n" +
+				$"{_city}, {_stateProvince}\n" +
+				$"{_country}";
+		}
+  }
+}
